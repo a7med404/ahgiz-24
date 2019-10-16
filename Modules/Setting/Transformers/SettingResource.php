0e2e30b8f40c228a901ef946eb40a21e6134ebca
a7med404 <a7med_404@outlook.com>
2019-10-16 16:24:25 +0200

    get-settings-stations

--- Modules/Setting/Transformers/SettingResource.php
+++ Modules/Setting/Transformers/SettingResource.php
@@ -14,6 +14,9 @@ class SettingResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name_setting' => $this->name_setting,
+            'value' => $this->value,
+        ];
     }
 }
